45746a34e89105a47d86c15e560e5ad37bdfe758
Florian Lenz <33814851+FlonairLenz@users.noreply.github.com>
2018-04-28 19:03:51 +0200

    KNN-Algorithm implemented
    No testing just implementation

--- knn_algorithm.py
+++ knn_algorithm.py
@@ -1,36 +1,40 @@
-function weight():
-    pass
+class DataPoint:
+    __init__(self, features, label):
+        self.features = features
+        self.label = label
+
+    setDistance(self, distance):
+        self.distance = distance
+
+    getDistance(self):
+        return self.distance
+
+function weight(d_j):
+    return 1 / d_j
 
 function distance(x_i, x_j):
-    pass
+    return sum([(x_i.features[i] - x_j.features[i]) ** 2 for i in range(len(x_i.features))]) ** 1 / 2 
 
-function knn(x_i, data):
-    pass
+function knn(k, x_i, dataPoints):
+    knearest = []
+    for x_j in dataPoints:
+        d_j = distance(x_i, x_j)
+        wd_j = weigth(d_j)
+        x_j.setDistance(wd_j)
 
-#function
-#distance(x_i, x_j)
-#d = 0
-#foreach
-#property in x_i
-#s = (x_i.property - x_j.property) ** 2
-#d += s
+        if len(knearest) < k:
+            knearest.append(x_j)
+        elif wd_j < knearest[-1].getDistance():
+            knearest[-1] = x_j
+            knearest.sort(key = lambda x: x.distance)
 
-#return d ** (1 / 2)
+    labels = {}
+    nearest = (None, 0)
+    for neighbor in knearest:
+        if neighbor.label in labels: labels[neighbor] += neighbor.distance
+        else: labels[neighbor] = neighbor.distance
 
-#function
-#knn(x_i, points)
-#k_nearest = []
-#foreach
-#x_j in points
-#d_j = distance(x_i, x_j)
-#if d_j < max(d(k_nearest))
-#    replace
-#    max(k_nearest)
-#    with x_j
-
-#category = {}
-#foreach
-#nearest in k_nearest
-#category[nearest] += 1
-
-#return max(category)
+        if labels[neighbor] > nearest[1]:
+            nearest = (neighbor, labels[neighbor])
+    
+    return nearest[0]
