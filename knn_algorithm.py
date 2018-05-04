f99fe4d21b6695f3cbeec8ee074f1fe20b91ab4c
Florian Lenz <33814851+FlonairLenz@users.noreply.github.com>
2018-05-04 14:26:04 +0200

    Update knn_algorithm.py

--- knn_algorithm.py
+++ knn_algorithm.py
@@ -1,5 +1,5 @@
 class DataPoint:
-    def __init__(self, features, label):
+    def __init__(self, features, label = None):
         self.features = features
         self.label = label
 
@@ -9,6 +9,12 @@ class DataPoint:
     def getDistance(self):
         return self.distance
 
+    def __iter__(self):
+        for f in self.features:
+            yield f
+
+        yield self.label
+
 def weight(d_j):
     return 1 / d_j
 
@@ -17,16 +23,17 @@ def distance(x_i, x_j):
 
 def knn(k, x_i, dataPoints):
     knearest = []
+
     for x_j in dataPoints:
         d_j = distance(x_i, x_j)
-        wd_j = weigth(d_j)
+        wd_j = weight(d_j)
         x_j.setDistance(wd_j)
 
         if len(knearest) < k:
             knearest.append(x_j)
-        elif wd_j < knearest[-1].getDistance():
+        elif wd_j > knearest[-1].getDistance():
             knearest[-1] = x_j
-            knearest.sort(key = lambda x: x.distance)
+            knearest.sort(key = lambda x: x.distance)   
 
     labels = {}
     nearest = (None, 0)
@@ -37,4 +44,4 @@ def knn(k, x_i, dataPoints):
         if labels[neighbor] > nearest[1]:
             nearest = (neighbor, labels[neighbor])
     
-    return nearest[0]
+    return nearest[0].label
